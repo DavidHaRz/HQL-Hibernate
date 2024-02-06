a521c85652753cf4946a74726c9e9f895ed6e567
David Horcajada Rodríguez <davidhorcajada36@gmail.com>
2024-02-06 20:30:51 +0100

    Práctica de HQL-Hibernate

---
+++ U3-ORM-HIBERNATE-UPDATE/src/main/java/David/U3_ORM_HIBERNATE_UPDATE/App.java
@@ -0,0 +1,13 @@
+package David.U3_ORM_HIBERNATE_UPDATE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
